2fe6b199a76483f18821d76820c10fe96b56ade8
Benoit Daccache <ben.daccache@gmail.com>
2014-03-19 17:17:00 +0100

    Moved UA and domain for analytics to config file

--- src/Main/WsApplication.cpp
+++ src/Main/WsApplication.cpp
@@ -289,12 +289,16 @@ const std::string& WsApplication::homePage()
 
 void WsApplication::googleAnalyticsLogger(std::string newPath)
 {
-    LOG(DEBUG)<<"WsApplication::googleAnalyticsLogger() : called ";
+    bool enableAnalytics = WsGlobalProperties::instance()->get("analytics", "enable", "false") == "true" ? true : false;
+    if(!enableAnalytics) return ;
+
+    std::string UA = WsGlobalProperties::instance()->get("analytics", "UA", "") ;
+    std::string domain = WsGlobalProperties::instance()->get("analytics", "domain", "none") ;
     std::string googleCmd = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\
                              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\
                              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\
                              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');\
-                             ga('create', 'UA-49163150-1', 'auto');\
+                             ga('create', '" + UA + "', '" + domain + "');\
                              ga('send', 'pageview');";
      doJavaScript(googleCmd);
 }
