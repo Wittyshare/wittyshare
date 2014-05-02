8d46e14a9cdfa1f9f6504b062d7a51fb97f7eced
Benoit Daccache <ben.daccache@gmail.com>
2014-05-02 12:37:39 +0200

    Finished Pdf renderer with page flip

---
+++ src/WsModules/WsModPdf/resources/WsModPdf.js
@@ -0,0 +1,60 @@
+//
+// Get page info from document, resize canvas accordingly, and render page
+//
+
+var pageNum = 1;
+var pdfjs = null;
+var pdfDoc = null;
+
+function load(url){
+
+    pdfjs = PDFJS.getDocument(url);
+    pdfjs.then(function getPdfHelloWorld(_pdfDoc) {
+        pdfDoc = _pdfDoc;
+        renderPage(pageNum);
+    });
+}
+
+function renderPage(num) {
+    pdfjs.then(function(pdf) {
+        pdf.getPage(num).then(function(page) {
+            var scale = 1;
+            var viewport = page.getViewport(scale);
+            var canvas = document.getElementById('the-canvas');
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+            var renderContext = {
+                canvasContext: context,
+        viewport: viewport
+            };
+            page.render(renderContext);
+        });
+    });
+    document.getElementById('page_num').textContent = pageNum;
+    document.getElementById('page_count').textContent = pdfDoc.numPages;
+}
+
+//
+// Go to previous page
+//
+function goPrevious() {
+    if (pageNum <= 1)
+        return;
+    pageNum--;
+    renderPage(pageNum);
+}
+
+//
+// Go to next page
+//
+function goNext() {
+    if (pageNum >= pdfDoc.numPages)
+        return;
+    pageNum++;
+    renderPage(pageNum);
+}
+
+//
+// Asynchronously download PDF as an ArrayBuffer
+//
