a3d9d055db162b694c0b19b77ef04c1e803c6f7b
Benoit Daccache <ben.daccache@gmail.com>
2014-05-14 10:23:23 +0200

    Moved widget id to config file

--- src/WsModules/WsModTwitterStream/src/WsModTwitterStream.cpp
+++ src/WsModules/WsModTwitterStream/src/WsModTwitterStream.cpp
@@ -1,12 +1,24 @@
-#include <Wt/WApplication>
+#include <dlfcn.h>
+#include <stdio.h>
+
+#include <iostream>
+
+#include <Include/WsGlobalConfig.h>
+
 #include <Wt/WLogger>
+#include <Wt/WTheme>
 #include <Wt/WText>
+
+#include <Main/WsApplication.h>
+
+#include <Wt/WApplication>
 #include <Wt/WTextArea>
 #include <Wt/WTimer>
 
 #include <Logger/WsLogger.h>
 
 #include "WsModTwitterStream.h"
+#include <boost/lexical_cast.hpp>
 
 using namespace Wt;
 
@@ -35,10 +47,18 @@ void WsTwitterStream::load()
 {
   Wt::WApplication::instance()->require("//platform.twitter.com/widgets.js");
   WContainerWidget::load();
+  std::string width = Wt::asString(option("width")).toUTF8();
+  std::string url = Wt::asString(option("url")).toUTF8();
+  std::string widgetId = Wt::asString(option("widgetId")).toUTF8();
+  std::string text = Wt::asString(option("text")).toUTF8();
   setId("twitterstream");
   //resize(WLength(25, WLength::Percentage), WLength(25, WLength::Percentage));
   setOverflow(WContainerWidget::OverflowAuto);
-  std::string html = "<a class=\"twitter-timeline\" data-dnt=\"true\" href=\"https://twitter.com/EUROFER_eu\" data-widget-id=\"441216909459984384\">Tweets by @EUROFER_eu</a>";
+  std::string html = "<a class=\"twitter-timeline\"";
+  if (width!="")
+    html+=" width=\""+width+"\"";
+  html+=" data-dnt=\"true\" href=\""+url+"\" data-widget-id=\""+widgetId+"\">"+text+"</a>";
+  wApp->log("notice") << "WsModTwitterStream::load() "+html;
   WText* textField = new WText(this);
   textField->setTextFormat(Wt::XHTMLUnsafeText);
   textField->setText(html);
